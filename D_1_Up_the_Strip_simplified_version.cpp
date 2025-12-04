13300e1c7d6a3ecefac19aed6845e9a0590fbaf7
rishi <rishirajspace@gmail.com>
2025-12-04 11:57:47 +0530

    first commit

---
+++ D_1_Up_the_Strip_simplified_version.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll long long
+
+// const int MOD = 1000000007; 
+// const int M = 1000000007;
+
+int MOD;
+int M;
+
+
+static int fact[1000005]; 
+static int invFact[1000005];
+
+long long mod(long long x) { return ((x % M + M) % M); }
+long long add(long long a, long long b) { return mod(mod(a) + mod(b)); }
+long long mul(long long a, long long b) { return mod(mod(a) * mod(b)); }
+
+ll modPow(ll a, ll b) {
+    if (b == 0) return 1LL;
+    if (b == 1) return a % M;
+    ll res = 1;
+    while (b) {
+        if (b % 2 == 1) res = mul(res, a);
+        a = mul(a, a);
+        b = b / 2;
+    }
+    return res;
+}
+
+ll inv(ll x) { return modPow(x, M - 2); }
+
+const int N = 1000005;
+
+void precompute() {
+    fact[0] = 1;
+    for (int i = 1; i < N; i++) {
+        fact[i] = mul(fact[i - 1], i);
+    }
+
+    invFact[N - 1] = inv(fact[N - 1]);        // inv of last factorial
+    for (int i = N - 2; i >= 0; i--) {
+        invFact[i] = mul(invFact[i + 1], i + 1);
+    }
+}
+
+
+
+
+void solve() {
+   /*
+        x se kahan ja skta hai
+        x/2 
+        ()
+ 
+   */
+    int n, m;
+    cin >> n >> m;
+    M = m;
+    MOD = m;
+    vector<int> dp(n + 1, 0);
+    int sum = 0;
+    dp[n] = 1;
+    
+    
+    for (int i = n; i >= 1; i--) {
+        dp[i] = add(dp[i], sum);
+        
+        int curr = i;
+        int div = 2;
+        
+        
+        sum = add(sum, dp[i]);
+    }
+    cout << dp[1] << endl;
+
+
+}
+
+signed main() {
+// #ifndef ONLINE_JUDGE
+//     freopen("inputf.txt", "r", stdin);
+//     freopen("outputf.txt", "w", stdout);
+//     freopen("error.txt", "w", stderr);
+// #endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T=1;
+    //cin >> T;
+    while (T--) solve();
+    return 0;
+}
